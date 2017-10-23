0f7a17ca44e75eda96a3fb76c3f023a961343c1c
maxb02 <maxbondar02@mail.com>
2017-10-23 18:04:49 +0300

    add db to gir ignore, separate setting.py

---
+++ pbsite/pbservicesite/local_setting.py
@@ -0,0 +1,18 @@
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'fajud^0-mou3m9b^(ytmz!)6!kef=rbm21y7stbpfr8l!z-8er'
+
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
