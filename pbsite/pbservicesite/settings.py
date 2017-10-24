d7b3b55409aebe8fab59460509a6d9f29c1b5795
Maksym Bondar <maxbondar02@gmail.com>
2017-10-24 13:27:13 +0000

    Update settings.py

--- pbsite/pbservicesite/settings.py
+++ pbsite/pbservicesite/settings.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fajud^0-mou3m9b^(ytmz!)6!kef=rbm21y7stbpfr8l!z-8er'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -82,17 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pbservicesite.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
