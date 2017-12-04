02bfdadc7be6fb033c934158df4b6815b45b83a7
Игорь Курганский <kyrgansky_i@pbi.office>
2017-12-04 10:44:01 +0200

    Delete /technicalguides/static/js folder; Remove custom js dropdown from base.html; Add hover dropdown in style.css;

--- pbsite/technicalguides/static/js/system-actions.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$( "li.dropdown" ).hover(function() {
-				$(this).dropdown('toggle');
-			}, function() {
-				$(this).parent().removeClass("show");
-				$(this).removeAttr("aria-expanded");
-				$(this).find('div.dropdown-menu').removeClass("show");
-		});
-});
