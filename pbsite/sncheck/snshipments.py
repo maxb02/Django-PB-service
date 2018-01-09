9abcb0ba110bceb9ecbf72c2a40a30fc0c787fec
Maksym Bondar <maxbondar02@gmail.com>
2018-01-09 14:31:39 +0000

    Update snshipments.py

--- pbsite/sncheck/snshipments.py
+++ pbsite/sncheck/snshipments.py
@@ -5,7 +5,7 @@ def sn_shipments(sn):
     if sn:
         url = 'http://gw.greendata.com.ua:8008/api/get-device-info'
         try:
-            info = requests.get(url,params={'sn': sn}, verify=False).json(
+            info = requests.get(url,params={'sn': sn}, verify=False).json()
         except:
             return 'Erorr'
         if info:
