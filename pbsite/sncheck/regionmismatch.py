58480522a90e100dc75da702515f41aa7b8a29d4
Maksym Bondar <maxbondar02@gmail.com>
2018-01-12 16:35:01 +0200

    ad <br> to notification

--- pbsite/sncheck/regionmismatch.py
+++ pbsite/sncheck/regionmismatch.py
@@ -8,7 +8,7 @@ def regionmismatch(serial_number, device_region, user, email_for_notifications_l
     username = user.username
     userfullname = user.get_full_name()
     usergroupslist = list(user.groups.values_list('name',flat=True))
-    html_content = "<b>SN:</b> {}<b>Device Region:</b> {} <b>User Name</b> {} {} <b>User Groups</b>: {}".format(serial_number, device_region, username, userfullname, usergroupslist)
+    html_content = "<b>SN:</b> {}<br><b>Device Region:</b> {} <br><b>User Name</b> {} {} <br><b>User Groups</b>: {}".format(serial_number, device_region, username, userfullname, usergroupslist)
     msg = EmailMessage(subject, html_content, from_email, to)
     msg.content_subtype = "html"  # Main content is now text/html
     msg.send()
