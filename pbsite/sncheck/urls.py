db20c0f76d3779f7be63663200b5d1509898f298
Maksym Bondar <maxbondar02@gmail.com>
2018-01-26 15:06:34 +0200

    merge sndetail & sncheck to one file

--- pbsite/sncheck/urls.py
+++ pbsite/sncheck/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^sncheck/$', views.serialcheck, name='sncheck'),
-    url(r'^sndetail', views.sndetail, name='sn_detail'),
+    url(r'^sncheck', views.serialcheck, name='sncheck'),
     ]
 
--- pbsite/sncheck/views.py
+++ pbsite/sncheck/views.py
@@ -8,19 +8,18 @@ from .models import EmailForNotifications
 
 @login_required
 def serialcheck(request):
-    return render(request, 'sncheck/sncheck.html')
+    if request.method == "POST":
+        serial_number = request.POST['sn'].strip().upper()
+        is_valid = snvalidator(serial_number)
+        info = sn_shipments(serial_number)
+        if not (request.user.groups.filter(name= 'moderator').exists() or request.user.is_superuser):
+            for data in info:
+                if not request.user.groups.filter(name= data['countryEng']).exists():
+                    regionmismatch(serial_number, data['countryEng'], request.user, list(EmailForNotifications.objects.all()))
 
-@login_required
-def sndetail(request):
-    serial_number = request.POST['sn'].strip().upper()
-    is_valid = snvalidator(serial_number)
-    info = sn_shipments(serial_number)
-    if not (request.user.groups.filter(name= 'moderator').exists() or request.user.is_superuser):
-        for data in info:
-            if not request.user.groups.filter(name= data['countryEng']).exists():
-                regionmismatch(serial_number, data['countryEng'], request.user, list(EmailForNotifications.objects.all()))
-
-    return render(request, 'sncheck/sndetail.html', {'serial_number': serial_number,
-                                             'is_valid': is_valid,
-                                             'info': info,
-                                                     })
+        return render(request, 'sncheck/sncheck.html', {'serial_number': serial_number,
+                                                 'is_valid': is_valid,
+                                                 'info': info,
+                                                         })
+    else:
+        return render(request, 'sncheck/sncheck.html')
