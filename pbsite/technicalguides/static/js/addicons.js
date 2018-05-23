1cef89364daa1796217e5537bc5399bf66d2cc14
maxb02 <maxbondar02@mail.com>
2018-05-23 18:14:40 +0300

    add image viewer for guide

--- pbsite/technicalguides/static/js/addicons.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() {
-	var matchTypes = "[href*='zip'], [href*='7z'], [href*='rar'], [href*='exe']";
-	$(".container a[href*='ftp'], "+matchTypes).prepend("<i class=\"fa fa-download\" aria-hidden=\"true\">&nbsp;");
-	$(".container a[href*='ftp'], "+matchTypes).append("</i>");
-	$(".container a[href*='http']:not("+matchTypes+", :has(img))").prepend("<i class =\"fa fa-external-link-square\" aria-hidden=\"true\">&nbsp;");
-	$(".container a[href*='http']:not("+matchTypes+", :has(img))").append("</i>");
-});
