58a87429d20672257b301f0b3d63d44f6a5dae25
Maksym Bondar <maxbondar02@gmail.com>
2018-05-26 18:06:13 +0300

    redo the region check and region mismatch notifier

--- pbsite/sncheck/region_checker.py
+++ pbsite/sncheck/region_checker.py
@@ -1,8 +1,12 @@
+from .models import AllowedDeviceRegion
 
-def region_ceker(user, ):
-    if not (request.user.groups.filter(
-            name='moderator').exists() or request.user.is_superuser or info == False or info == 'Erorr'):
-        for data in info:
-            if not request.user.groups.filter(name=data['countryEng']).exists():
-                regionmismatch(serial_number, data['countryEng'], request.user,
-                               list(EmailForNotifications.objects.all()))
+
+def region_ceker(user, device_info):
+    if (user.groups.filter(
+            name='moderator').exists() or user.is_superuser or device_info == False or device_info == 'Erorr'):
+        return True
+    if not device_info or device_info == 'Erorr':
+        for device_data in device_info:
+            if AllowedDeviceRegion.objects.filter(
+                    user_region=user.groups.all(), device_region=device_data['countryEng']).exists():
+                return True
--- pbsite/sncheck/views.py
+++ pbsite/sncheck/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .snshipments import *
-from .snvalidator import *
-from .region_mistmach_notifier import *
+from .snshipments import sn_shipments
+from .snvalidator import snvalidator
+from .region_checker import region_ceker
+from .region_mismatch_notifier import region_mistmatch_notifier
 
-from .models import EmailForNotifications
 
 @login_required
 def serialcheck(request):
     if request.method == "POST":
         serial_number = request.POST['sn'].strip().upper()
         is_valid = snvalidator(serial_number)
-        info = sn_shipments(serial_number)
-
+        device_info = sn_shipments(serial_number)
+        if not region_ceker(request.user, device_info):
+            region_mistmatch_notifier(serial_number, device_info[0], request.user, request.LANGUAGE_CODE)
 
         return render(request, 'sncheck/sncheck.html', {'serial_number': serial_number,
                                                  'is_valid': is_valid,
-                                                 'info': info,
-                                                         })
+                                                        'device_info': device_info,
+                                                        })
     else:
         return render(request, 'sncheck/sncheck.html')
