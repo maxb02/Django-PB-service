7ba0da9a9cedd61572990bbb90067c5b767ed9db
Maksym Bondar <maxbondar02@gmail.com>
2018-06-11 15:22:47 +0300

    for migtate

--- pbsite/users/models.py
+++ pbsite/users/models.py
@@ -2,11 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from servicecenters.models import ServiceCenter
 
+
 class User(AbstractUser):
     # service_center = models.ForeignKey(ServiceCenter, blank=True, null=True)
-    pass
     class Meta:
         db_table = 'auth_user'
-
-
-
