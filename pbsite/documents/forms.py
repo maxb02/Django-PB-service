f6dc75b84378c06d5c98032534006b1ad65d2107
maxb02 <maxbondar02@gmail.com>
2018-09-28 17:56:50 +0300

    commit

--- pbsite/documents/forms.py
+++ pbsite/documents/forms.py
@@ -1,9 +1,19 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Act
+
 
 class ActRequestForm(ModelForm):
     class Meta:
         model = Act
-        # fields = [serial_number, received_date, purchase_date, is_presale, document_type,
-        #           client_name, seller_name, document_photo, screen_photo, defect_photo]
-        fields = '__all__'
+        fields = ('serial_number', 'received_date', 'purchase_date', 'is_presale', 'customers_claim', 'identified_malfunction', 'document_type',
+                  'client_name', 'seller_name', 'warranty_card_photo', 'receipt_photo', 'screen_photo', 'defect_photo')
+        widgets = {
+            'received_date': forms.DateInput(format=('%d/%m/%Y'),
+                                             attrs={'class': 'form-control', 'placeholder': 'Select a date',
+                                                    'type': 'date'}),
+            'purchase_date': forms.DateInput(format=('%d/%m/%Y'),
+                                             attrs={'class': 'form-control', 'placeholder': 'Select a date',
+                                                    'type': 'date'}),
+        }
+
--- pbsite/documents/models.py
+++ pbsite/documents/models.py
@@ -8,16 +8,23 @@ def upload_path_handler(self, filename):
                                                          file=filename)
 
 class Act(models.Model):
-    serial_number = models.CharField(max_length=50, verbose_name ='Serial Number')
+    serial_number = models.CharField(max_length=20, verbose_name ='Serial Number')
     added_by = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name ='User Name')
     filling_date = models.DateTimeField(blank=True, auto_now_add=True)
-    purchase_date = models.DateTimeField(verbose_name ='Purchase Date')
-    received_date = models.DateTimeField(verbose_name ='Received Date')
+    purchase_date = models.DateField(verbose_name ='Purchase Date')
+    received_date = models.DateField(verbose_name ='Received Date')
     conclusion_date = models.DateTimeField(null=True, blank=True, verbose_name='Conclusion Date')
-    is_presale = models.BooleanField(verbose_name='It is presale?')
-    document_photo = models.ImageField(null=True, blank=True, upload_to=upload_path_handler, verbose_name='Photo of warranty card or receipt')
+    is_presale = models.BooleanField(verbose_name='It is presale')
+
+    customers_claim = models.CharField(max_length = 50, verbose_name ='Customers Claim')
+    identified_malfunction = models.CharField(max_length = 50, verbose_name ='Identified Malfunction')
+
+    warranty_card_photo = models.ImageField(null=True, blank=True, upload_to=upload_path_handler, verbose_name='Photo of warranty card or receipt')
+    receipt_photo = models.ImageField(null=True, blank=True, upload_to=upload_path_handler,verbose_name='Photo of warranty card or receipt')
     screen_photo = models.ImageField(null=True, blank=True, upload_to=upload_path_handler, verbose_name='Photo of screen')
     defect_photo = models.ImageField(null=True, blank=True, upload_to=upload_path_handler, verbose_name='Photo of defect')
+
+
     client_name = models.CharField(max_length=50, verbose_name ='Client Name')
     seller_name= models.CharField(max_length=50, verbose_name='Seller Name')
     document_type = models.CharField(max_length=100, verbose_name='Type',choices=(
@@ -31,6 +38,10 @@ class Act(models.Model):
                                                         ('confirmed', 'Confirmed'),
                                                         ('rejected', 'Rejected'),
                                                     ))
+
+    comment_of_engineer =
+
+
 
     def save_model(self, request, obj, form, change):
         obj.added_by = request.user
