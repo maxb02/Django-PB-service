7de3b88b5d125bc21a6416217fde77f3febeadc7
bondar_m <maxbondar02@hmail.com>
2019-02-13 16:56:42 +0200

    refactor guides template and css

--- pbsite/technicalguides/static/js/guide.js
+++ pbsite/technicalguides/static/js/guide.js
@@ -1,24 +1,25 @@
-$(document).ready( function() {
+$(document).ready(function () {
 
 
-	var initAddIcons = function () {
-		var matchTypes = "[href*='zip'], [href*='7z'], [href*='rar'], [href*='exe']";
-		$(".container a[href*='ftp'], "+matchTypes).prepend("<i class=\"fa fa-download\" aria-hidden=\"true\">&nbsp;");
-		$(".container a[href*='ftp'], "+matchTypes).append("</i>");
-		$(".container a[href*='http']:not("+matchTypes+", :has(img))").prepend("<i class =\"fa fa-external-link-square\" aria-hidden=\"true\">&nbsp;");
-		$(".container a[href*='http']:not("+matchTypes+", :has(img))").append("</i>");
-	}
+    var initAddIcons = function () {
+        var matchTypes = "[href*='zip'], [href*='7z'], [href*='rar'], [href*='exe']";
+        $(".container a[href*='ftp'], " + matchTypes).prepend("<i class=\"fa fa-download\" aria-hidden=\"true\">&nbsp;");
+        $(".container a[href*='ftp'], " + matchTypes).append("</i>");
+        $(".container a[href*='http']:not(" + matchTypes + ", :has(img))").prepend("<i class =\"fa fa-external-link-square\" aria-hidden=\"true\">&nbsp;");
+        $(".container a[href*='http']:not(" + matchTypes + ", :has(img))").append("</i>");
+    }
 
-	var initFavcyBoxImg = function () {
+    var initFavcyBoxImg = function () {
         $(".box img").each(function () {
             $(this).fancybox({
                 //'hideOnContentClick': true,
                 'href': $(this).attr('src')
             });
         });
-	}
+    }
 
-	initAddIcons();
-	initFavcyBoxImg();
+    initAddIcons();
+    initFavcyBoxImg();
+
 
 });
