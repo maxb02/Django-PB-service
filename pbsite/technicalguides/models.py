eaec713518e40d30e929fd364bcbb0db3d89b559
maxb02 <maxbondar02@gmail.com>
2020-08-12 15:43:27 +0300

    add new device model foreign key to technicalguides

--- pbsite/technicalguides/models.py
+++ pbsite/technicalguides/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import Group
 from ckeditor_uploader.fields import RichTextUploadingField
+from device.models import Device as NewDevice
 
 
 class Device(models.Model):
@@ -27,6 +28,7 @@ class Guide(models.Model):
     published_date = models.DateTimeField(blank=True, auto_now_add=True)
     updated_date = models.DateTimeField(blank=True, auto_now=True)
     objects = GuideManager()
+    _device = models.ForeignKey(NewDevice, null=True)
 
     def __str__(self):
         return self.title
