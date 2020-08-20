f80b560788725d16f53e79ac6c15af0fec755c08
maxb02 <maxbondar02@gmail.com>
2020-08-20 13:21:30 +0300

    add number field to documents act admin

--- pbsite/documents/admin.py
+++ pbsite/documents/admin.py
@@ -5,10 +5,11 @@ from .models import Act, SerialNumberPrefix, VisualDefect, ScopeOfSupply
 
 @admin.register(Act)
 class ActAdmin(admin.ModelAdmin):
-    list_display = (
-    'get_region', 'get_service', 'get_model', 'serial_number', 'document_type', 'status', 'filling_date',
-    'received_date', 'purchase_date')
-
+    list_display = ('number',
+                    'get_region', 'get_service', 'get_model', 'serial_number', 'document_type', 'status',
+                    'filling_date',
+                    'received_date', 'purchase_date')
+    search_fields = 'number', 'serial_number'
 
 
 @admin.register(SerialNumberPrefix)
@@ -18,9 +19,13 @@ class SerialNumberPrefixAdmin(admin.ModelAdmin):
 
 class ScopeOfSupplyAdmin(TranslationAdmin):
     pass
+
+
 admin.site.register(ScopeOfSupply, ScopeOfSupplyAdmin)
+
 
 class VisualDefectAdmin(TranslationAdmin):
     pass
+
 
 admin.site.register(VisualDefect, VisualDefectAdmin)
