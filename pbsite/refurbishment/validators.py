abb316dc9fd0c6e54ef9ce5a51b2b92b887ded7f
maxb02 <maxbondar02@gmail.com>
2021-02-04 11:53:55 +0200

    fix clock line issue detail page title

--- pbsite/refurbishment/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-# from
-#
-# def refurbishment_validator(refurbishments):
-#     for refurbishment in refurbishments:
-#         print(refurbishments.name)
