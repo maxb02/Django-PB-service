338abdcae73271dfa6d3be93ffd6addc94c4d2f8
maxb02 <maxbondar02@gmail.com>
2021-02-09 17:54:20 +0200

    change default language

--- pbsite/pbservicesite/settings.py
+++ pbsite/pbservicesite/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 from django.utils.translation import ugettext_lazy as _
 
