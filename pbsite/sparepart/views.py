785d1390c77d74fe5f2188f39abff90d3ff7e419
maxb02 <maxbondar02@gmail.com>
2021-04-06 17:45:55 +0300

    add device spare part list page

--- pbsite/sparepart/views.py
+++ pbsite/sparepart/views.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, View
 from .models import SparePart
+from device.models import Device
+
 
 class SparePartDetail(LoginRequiredMixin, DetailView):
     model = SparePart
+
+
+class SparePartDeviceList(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        device = get_object_or_404(Device.objects.prefetch_related('spare_parts'), pk=pk)
+        print(device)
+        return render(request, 'sparepart/sparepart_list.html', {
+            'device': device,
+        })
