c30f678418382b70968f5e8378f3c26a5f36179b
maxb02 <maxbondar02@gmail.com>
2021-04-08 19:53:25 +0300

    fix document url '/'

--- pbsite/documents/urls.py
+++ pbsite/documents/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^request$', views.ActCreate.as_view(), name='document_request_url'),
     url(r'^list', views.ActList.as_view(), name='document_list_url'),
-    url(r'^/detail/(?P<number>.+)$', views.ActDetail.as_view(), name='document_detail_url'),
+    url(r'^detail/(?P<number>.+)$', views.ActDetail.as_view(), name='document_detail_url'),
     url(r'^accept_or_reject/(?P<number>.+)$', views.ActAcceptOrReject.as_view(),
         name='document_accept_or_reject_url'),
     url(r'^compensation_status/(?P<pk>.+)$', views.ActCompensationStatusUpdate.as_view(),
