ca3612df9ab4d7d41394be42c45c30743108846f
maxb02 <maxbondar02@gmail.com>
2021-04-09 20:21:00 +0300

    update spare parts list page

--- pbsite/sparepart/views.py
+++ pbsite/sparepart/views.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, View
@@ -11,9 +13,13 @@ class SparePartDetail(LoginRequiredMixin, DetailView):
 
 class SparePartDeviceList(LoginRequiredMixin, View):
     def get(self, request, pk):
-        device = get_object_or_404(Device.objects.prefetch_related('spare_parts'), pk=pk)
-        categorys = Category.objects.filter(spare_parts__device__pk=device.pk)
+        device = get_object_or_404(Device.objects.prefetch_related('spare_parts__category'), pk=pk)
+        categories = defaultdict(list)
+
+        for spare_part in device.spare_parts.all():
+            categories[spare_part.category.name].append(spare_part)
+
         return render(request, 'sparepart/sparepart_list.html', {
             'device': device,
-            'categorys': categorys,
+            'categories': dict(categories),
         })
