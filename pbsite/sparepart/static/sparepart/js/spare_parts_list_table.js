80a3d3a0a40ddf261b268436382113cf14939b8f
maxb02 <maxbondar02@gmail.com>
2021-04-20 15:24:14 +0300

    fix spare parts orders in list

--- pbsite/sparepart/static/sparepart/js/spare_parts_list_table.js
+++ pbsite/sparepart/static/sparepart/js/spare_parts_list_table.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#spare_parts_table').DataTable( {
-         "order": [[ 1, "desc" ]],
+         "order": [[ 2, "desc" ]],
         dom: 'Bfrtip',
         buttons: [
             'copy', 'excel', 'print',
