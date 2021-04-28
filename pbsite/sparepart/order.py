da7aade3dd2e2090d40c17e2384798cf0e4dce64
maxb02 <maxbondar02@gmail.com>
2021-04-28 14:27:41 +0300

    refactor order creation

--- pbsite/sparepart/order.py
+++ pbsite/sparepart/order.py
@@ -10,25 +10,21 @@ class CartIsEmptyException(Exception):
     pass
 
 
-def create_order(request):
-    cart = Cart(request)
+def create_order(cart, user, destination):
     if cart:
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            order.created_by = request.user
-            order.save()
-            for supplier_name, spare_parts_list in cart.get_items_by_supplier().items():
-                order_supplier = OrderSupplier.objects.create(
-                    order=order,
-                    supplier=Supplier.objects.get(name=supplier_name), )
-                for item in spare_parts_list:
-                    OrderItem.objects.create(order_supplier=order_supplier,
-                                             spare_part=item['spare_part'],
-                                             price=item['price'],
-                                             quantity=item['quantity'])
-            cart.clear()
-            return order
+        order = Order.objects.create(created_by=user,
+                                     destination=destination)
+        for supplier_name, spare_parts_list in cart.get_items_by_supplier().items():
+            order_supplier = OrderSupplier.objects.create(
+                order=order,
+                supplier=Supplier.objects.get(name=supplier_name))
+            for item in spare_parts_list:
+                OrderItem.objects.create(order_supplier=order_supplier,
+                                         spare_part=item['spare_part'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+        cart.clear()
+        return order
     raise CartIsEmptyException
 
 
